913173207563b61a5a2b0330dad62a0bb8a936a8
Ahmed Nagi <ahmedflnagi@gmail.com>
2025-08-26 15:51:27 +0300

    add tailwind

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}', './src/app/**/*.{ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+} satisfies Config;
